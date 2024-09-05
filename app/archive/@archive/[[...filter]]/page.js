9ae95e5ee00401a77a9a7bcf453ae4a81f19503a
Khouloud Ben Salem <khouloud.bensalem@ensi-uma.tn>
2024-09-05 12:15:47 +0200

    catch all fallback routes & dealing with multiple path segments

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,64 @@
+import Link from 'next/link';
+
+import NewsList from '@/components/news-list';
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from '@/lib/news';
+
+export default function FilteredNewsPage({ params }) {
+  const filter = params.filter;
+
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears(); //return list of years
+
+  //if we have selected year but not month
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear); // return list of months related to that selected year
+  }
+
+  //if we have selected both year and month we will not display the list of links
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for the selected period.</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+ // const href = selectedYear
+  //              ? `/archive/${selectedYear}/${link}`   if you select a year then you will be redirected to  /archive/2024/month  => link=month
+  //              : `/archive/${link}`;                  if you did not select a year then you'll be redirected to /archive/year  => link=year
+
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
+}
